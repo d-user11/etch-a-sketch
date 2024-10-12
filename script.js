19cc2f50b07bda0e087fd3f1d473f05673956f3f
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-10-12 17:36:45 +0300

    feat: Make the first version
    - Create a webpage with a 16x16 grid of square divs
    - Set up a "hover" effect so that the grid divs change color when mouse passes over them
    - Add a button on the top of the screen that sends the user a popup asking for the number of squares per side for the new grid

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('Etch A Sketch')
+const container = document.querySelector('div.container');
+const button = document.querySelector('button');
+
+button.addEventListener('click', resizeGrid);
+
+drawGrid(16);
+
+function drawGrid(gridSize) {
+    let squareSize = 100 / gridSize;
+    console.log(`Square size is ${squareSize}%`);
+
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        const square = document.createElement('div');
+        square.className = 'item';
+        square.style.width = `${squareSize}%`
+        square.addEventListener('mousemove', changeColor);
+        container.appendChild(square);
+    }
+
+}
+
+function changeColor() {
+    this.style.backgroundColor = 'black';
+}
+
+function resizeGrid() {
+    let gridSize = prompt('How many squares per side would you like?');
+    if (gridSize > 0 && gridSize <= 100) {
+        removeGrid();
+        drawGrid(gridSize);
+    } else {
+        alert("Can't resize grid! Maximum is 100 squares per side!");
+    }
+}
+
+function removeGrid() {
+    const squares = container.querySelectorAll('div.item');
+    for (square of squares) {
+        container.removeChild(square);
+    }
+}
